baab83f9995004ffb938aec9fbd60273ef21d72c
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-05-19 22:58:52 +0300

    Adjusted code to pe8 standard

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,9 +3,10 @@ import sys
 
 
 def safe_function(fct, *args):
+    """Execute functions safely and prints error messages, if any, to stderr"""
+
     try:
-        res = fct(*args)
-        return res
-    except Exception as err:
-        sys.stdout.write("Exception: " + str(err) + "\n")
-        return
+        return fct(*args)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return None
