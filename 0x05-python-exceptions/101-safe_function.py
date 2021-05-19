78d897812c9892c31b961f6ce74efe64e4badf71
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-05-19 22:54:43 +0300

    a function that executes a function safely.
    Prototype: def safe_function(fct, *args):

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,2 +1,11 @@
-#!usr/bin/bash
+#!/usr/bin/python3
+import sys
 
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+        return res
+    except Exception as err:
+        sys.stdout.write("Exception: " + str(err) + "\n")
+        return
