e6720f1e010b8633eee3c40ef3bddc484eae89c3
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2021-08-02 23:23:37 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-var args = process.argv;
+const process = require('process');
 
-if (args.length === 3) {
-  console.log('Argument found');
-}
-if (args.length === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
 }
-if (args.length > 3) {
+if (process.argv.length === 3) {
+  console.log('Argument found');
+}
+if (process.argv.length > 3) {
   console.log('Arguments found');
 }
