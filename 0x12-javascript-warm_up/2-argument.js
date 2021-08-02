c118167552d64e4c62d619deabf1b0e2c511dc7e
zidanegimiga <zidanegimiga@gmail.com>
2021-08-02 20:19:57 +0000

    An argument-counting script

---
+++ 0x12-javascript-warm_up/2-argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+var args = process.argv;
+
+if (args.length === 3){
+	console.log('Argument found');
+}
+if (args.length === 2){
+	console.log('No argument');
+}
+if (args.length > 3){
+	console.log('Arguments found');
+}
+
